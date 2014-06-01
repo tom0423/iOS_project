dabf249540c5bcf45f29645a1ddf8ce0c10501a4
SeonRan <yn2835@hanmail.net>
2014-06-01 19:25:51 +0900

    1. database에 contents table의 instance 추가 2. database에 저장되어있는 contents table에서 원하는 리스트를 반환하는 함수 추가 - (NSMutableArray*) getContentsList:(int)bookNum:(int)chapterNum ;       // N권 N장안의 컨텐츠 리스트들 검색하여 컨텐츠 리스트 반환 - (NSMutableArray*) getContentsURLList:(int)bookNum:(int)chapterNum ;    // N권 N장안의 컨텐츠 리스트들 검색하여 URL 리스트 반환

--- iosDic/iosDic/DicDataBase.h
+++ iosDic/iosDic/DicDataBase.h
@@ -12,14 +12,17 @@
 @interface DicDataBase : NSObject
 {
     NSMutableArray *ContentsListArray ; // 검색된 게시글의 내용을 저장하는 Array 객체
-    NSMutableArray *ChapterListArray;          // chapter의 이름을 저장하는 Array 객체
+    NSMutableArray *ContentsURLListArray ; // 검색된 게시글의 URL을 저장하는 Array 객체
+    NSMutableArray *ChapterListArray;   // chapter의 이름을 저장하는 Array 객체
 }
 
 - (void) DataBaseConnection:(sqlite3 **)tempDataBase ;  // 데이터베이스 연결
-- (void) getContentsList ;                              // N권 N장안의 컨텐츠 리스트들 검색
-- (NSMutableArray*) getChapterList:(int)bookNum ;        // N권 안의 chapter name 리스트들 검색
+- (NSMutableArray*) getContentsList:(int)bookNum:(int)chapterNum ;       // N권 N장안의 컨텐츠 리스트들 검색하여 컨텐츠 리스트 반환
+- (NSMutableArray*) getContentsURLList:(int)bookNum:(int)chapterNum ;    // N권 N장안의 컨텐츠 리스트들 검색하여 URL 리스트 반환
+- (NSMutableArray*) getChapterList:(int)bookNum ;       // N권 안의 chapter name 리스트들 검색
 
 @property(nonatomic, retain) NSMutableArray *ContentsListArray ;
+@property(nonatomic, retain) NSMutableArray *ContentsURLListArray ;
 @property(nonatomic, retain) NSMutableArray *ChapterListArray ;
 
 @end
--- iosDic/iosDic/DicDataBase.m
+++ iosDic/iosDic/DicDataBase.m
@@ -26,59 +26,7 @@
     }
 }
 
-// Contents 테이블에서 원하는 권, 장에대한 모든 컨텐츠 리스트들을 가져온다
-- (void)getContentsList
-{/*
-    NSString *pCName ;      // CName  : 파일명
-    NSString *pCField ;     // CField : 파일내용
-    NSNumber *pCNum ;       // CNum   : 파일넘버 (몇번째 파일?)
-    
-    sqlite3 *pDataBase ;
-    sqlite3_stmt *statement = nil ;
-    
-    [self DataBaseConnection:&pDataBase] ;  // 데이터베이스 연결
-    if( pDataBase == nil ) {
-        NSLog(@"Error DB : '%s'",sqlite3_errmsg(pDataBase)) ;
-        return ;
-    }
-    
-   
-    // 검색 SQL
- /*
-    int bookNum = 1;
-    const char *sql = "SELECT chName FROM Dictionary where Book = bookNum" ;
-    
-    // SQL Text를 prepared statement로 변환
-    if ( sqlite3_prepare_v2(pDataBase, sql, -1, &statement, NULL) != SQLITE_OK ) {
-        
-        NSLog(@"Error DB : '%s'", sqlite3_errmsg(pDataBase)) ;
-        sqlite3_close(pDataBase) ;
-        pDataBase = nil ;
-        return ;
-    }
-    
-   //
-    if ( ContentsListArray == nil ) {
-        ContentsListArray = [[NSMutableArray alloc] init ] ;
-    }
-    
-    // 객체에 등록
-    if ( ContentsListArray != nil ) {
-        [ContentsListArray removeAllObjects] ;
-        
-        // 쿼리를 실행
-        while ( sqlite3_step(statement) == SQLITE_ROW ) {
-            // p279
-        }
-        
-    }
-    
-    sqlite3_reset(statement) ;
-    sqlite3_finalize(statement) ;
-    sqlite3_close(pDataBase) ;
-    pDataBase = nil ;*/
-    
-}
+
 
 // N권 안의 chapter name 리스트들을 가져온다
 - (NSMutableArray*)getChapterList:(int)bookNum
@@ -111,6 +59,70 @@
     }
     
     
+    if ( ChapterListArray == nil ) {
+        ChapterListArray = [[NSMutableArray alloc] init ] ;
+    }
+    
+    // 객체에 등록
+    if ( ChapterListArray != nil ) {
+        [ChapterListArray removeAllObjects] ;
+        
+        // 쿼리를 실행
+        while ( sqlite3_step(statement) == SQLITE_ROW ) {
+            // 각 권에 대한 장 이름
+            pDName = [[NSString alloc] initWithString:[NSString stringWithUTF8String:(char*) sqlite3_column_text(statement, 0)]];
+            
+            // 검색 결과를 Array 객체에 담습니다.
+            //[ChapterListArray addObject:[NSDictionary dictionaryWithObjectsAndKeys:pDName, @"ChapterName", nil]];
+            [ChapterListArray addObject:pDName];
+        }
+        
+    }
+    
+    sqlite3_reset(statement) ;
+    sqlite3_finalize(statement) ;
+    sqlite3_close(pDataBase) ;
+    pDataBase = nil ;
+    
+    return ChapterListArray ;
+    
+}
+
+
+// N권 N장안의 컨텐츠 리스트들 검색하여 컨텐츠 리스트 반환
+- (NSMutableArray*) getContentsList:(int)bookNum:(int)chapterNum ;
+{
+    NSString *pCName ;      // CName  : 파일명
+    //NSString *pCField ;     // CField : 파일내용
+    //NSNumber *pCNum ;       // CNum   : 파일넘버 (몇번째 파일?)
+    sqlite3 *pDataBase ;
+    sqlite3_stmt *statement = nil ;
+    
+    ContentsListArray = [[NSMutableArray alloc] init ] ;
+    
+    // 데이터베이스 연결
+    [self DataBaseConnection:&pDataBase] ;
+    
+    if( pDataBase == nil ) {
+        NSLog(@"Error DB : '%s'",sqlite3_errmsg(pDataBase)) ;
+        return NULL ;
+    }
+    
+    // 검색 SQL
+    
+    NSString *querySQL = [NSString stringWithFormat:@"SELECT Contents.CName FROM Dictionary natural join Contents where Dictionary.Book = \"%d\" and Dictionary.Chapter = \"%d\" order by CNum", bookNum, chapterNum];
+    const char *sql = [querySQL UTF8String];
+    
+    // SQL Text를 prepared statement로 변환
+    if ( sqlite3_prepare_v2(pDataBase, sql, -1, &statement, NULL) != SQLITE_OK ) {
+        
+        NSLog(@"Error DB : '%s'", sqlite3_errmsg(pDataBase)) ;
+        sqlite3_close(pDataBase) ;
+        pDataBase = nil ;
+        return NULL ;
+    }
+    
+    
     if ( ContentsListArray == nil ) {
         ContentsListArray = [[NSMutableArray alloc] init ] ;
     }
@@ -122,11 +134,10 @@
         // 쿼리를 실행
         while ( sqlite3_step(statement) == SQLITE_ROW ) {
             // 각 권에 대한 장 이름
-            pDName = [[NSString alloc] initWithString:[NSString stringWithUTF8String:(char*) sqlite3_column_text(statement, 0)]];
+            pCName = [[NSString alloc] initWithString:[NSString stringWithUTF8String:(char*) sqlite3_column_text(statement, 0)]];
             
             // 검색 결과를 Array 객체에 담습니다.
-            //[ChapterListArray addObject:[NSDictionary dictionaryWithObjectsAndKeys:pDName, @"ChapterName", nil]];
-           [ChapterListArray addObject:pDName];
+            [ContentsListArray addObject:pCName];
         }
         
     }
@@ -136,7 +147,71 @@
     sqlite3_close(pDataBase) ;
     pDataBase = nil ;
     
-    return ChapterListArray ;
+    return ContentsListArray ;
+    
+}
+
+
+
+// N권 N장안의 컨텐츠 리스트들 검색하여 URL 리스트 반환
+- (NSMutableArray*) getContentsURLList:(int)bookNum:(int)chapterNum ;
+{
+    //NSString *pCName ;      // CName  : 파일명
+    NSString *pCField ;     // CField : 파일내용
+    //NSNumber *pCNum ;       // CNum   : 파일넘버 (몇번째 파일?)
+    sqlite3 *pDataBase ;
+    sqlite3_stmt *statement = nil ;
+    
+    ContentsURLListArray = [[NSMutableArray alloc] init ] ;
+    
+    // 데이터베이스 연결
+    [self DataBaseConnection:&pDataBase] ;
+    
+    if( pDataBase == nil ) {
+        NSLog(@"Error DB : '%s'",sqlite3_errmsg(pDataBase)) ;
+        return NULL ;
+    }
+    
+    // 검색 SQL
+    
+    NSString *querySQL = [NSString stringWithFormat:@"SELECT Contents.CField FROM Dictionary natural join Contents where Dictionary.Book = \"%d\" and Dictionary.Chapter = \"%d\" order by CNum", bookNum, chapterNum];
+    const char *sql = [querySQL UTF8String];
+    
+    // SQL Text를 prepared statement로 변환
+    if ( sqlite3_prepare_v2(pDataBase, sql, -1, &statement, NULL) != SQLITE_OK ) {
+        
+        NSLog(@"Error DB : '%s'", sqlite3_errmsg(pDataBase)) ;
+        sqlite3_close(pDataBase) ;
+        pDataBase = nil ;
+        return NULL ;
+    }
+    
+    
+    if ( ContentsURLListArray == nil ) {
+        ContentsURLListArray = [[NSMutableArray alloc] init ] ;
+    }
+    
+    // 객체에 등록
+    if ( ContentsURLListArray != nil ) {
+        [ContentsURLListArray removeAllObjects] ;
+        
+        // 쿼리를 실행
+        while ( sqlite3_step(statement) == SQLITE_ROW ) {
+            // 각 권에 대한 장 이름
+            pCField = [[NSString alloc] initWithString:[NSString stringWithUTF8String:(char*) sqlite3_column_text(statement, 0)]];
+            
+            // 검색 결과를 Array 객체에 담습니다.
+            [ContentsURLListArray addObject:pCField];
+        }
+        
+    }
+    
+    sqlite3_reset(statement) ;
+    sqlite3_finalize(statement) ;
+    sqlite3_close(pDataBase) ;
+    pDataBase = nil ;
+    
+    return ContentsURLListArray;
     
 }
 
